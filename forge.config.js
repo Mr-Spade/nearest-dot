7ce1bffc1a92f407986cee3cf6f5dc4ebd0fe542
Mr_Spade <Mr_Spade@foxmail.com>
2023-03-13 20:21:16 +0800

    change make platform

--- forge.config.js
+++ forge.config.js
@@ -2,21 +2,25 @@ module.exports = {
   packagerConfig: {},
   rebuildConfig: {},
   makers: [
-    // {
-    //   name: '@electron-forge/maker-squirrel',
-    //   config: {},
-    // },
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
     {
       name: '@electron-forge/maker-zip',
       platforms: ['win32','linux','darwin'],
     },
-    // {
-    //   name: '@electron-forge/maker-deb',
-    //   config: {},
-    // },
-    // {
-    //   name: '@electron-forge/maker-rpm',
-    //   config: {},
-    // },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-dmg',
+      config: {}
+    }
   ],
 };
