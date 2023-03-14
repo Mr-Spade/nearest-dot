8a878dac8bee447ccbdbdeae6fca158e3917ad07
Mr_Spade <Mr_Spade@foxmail.com>
2023-03-14 08:52:21 +0800

    fix msi

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { app, BrowserWindow } = require('electron')
 
+if(require('electron-squirrel-startup'))
+  app.quit();
+
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 1024,
